b2f6bb06808d7850a29cb05e07272f47230d619f
RamonRS <celmon1@gmail.com>
2023-09-15 12:58:52 +0200

    Ejemplos 15 Septiembre

---
+++ 15Septiembre_01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15Septiembre_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Programa que lee dos números y calcula la suma
+            //Declaración de variables:
+            int a, b, suma;
+            //  System.Int32 a1, b1, suma1; No es importante, solo para fardar con los colegas
+            //Inicializar a y b
+            a = 9;
+            b = 3;
+            //Calcular la suma
+            suma = a + b;
+            
+            //Escribir el resultado
+            Console.WriteLine("La suma es :"  + suma);
+            Console.ReadLine();
+
+
+        }
+       
+    }
+}
---
+++ 15Septiembre_02VB/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Programa que lee dos números y calcula la suma
+        'Declaración de variables
+        Dim a, b, suma As System.Int32
+        'Inicializar a y b
+        a = 9
+        b = 3
+        'Calcular la suma
+        suma = a + b
+
+        Console.WriteLine("La suma es : " & suma)
+        Console.ReadLine()
+    End Sub
+
+End Module
