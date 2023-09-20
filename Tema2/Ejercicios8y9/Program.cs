0ff72c3e82c2bf040178f46e710772bcdcd0f54e
RamonRS <celmon1@gmail.com>
2023-09-20 13:01:24 +0200

    Dia20Septbre

---
+++ Tema2/Ejercicios8y9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios8y9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("     *");
+            Console.WriteLine("  *     *");
+            Console.WriteLine("*    *     *");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tema3/01_TiposVariables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_TiposVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración de variables
+            byte n1;
+           // byte n2 = n1;
+
+        }
+    }
+}
