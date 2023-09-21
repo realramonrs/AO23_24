ddf8aacf61e6daf76f0796e48d05a59385e3732c
RamonRS <celmon1@gmail.com>
2023-09-21 11:03:08 +0200

    Dia 21 Septiembre

--- Tema3/01_TiposVariables/Program.cs
+++ Tema3/01_TiposVariables/Program.cs
@@ -12,7 +12,42 @@ namespace _01_TiposVariables
         {
             //Declaración de variables
             byte n1;
-           // byte n2 = n1;
+            //Inicializar una variable
+            n1 = 10;
+            byte n2 = n1;
+
+            //Declarar varias variables en la misma linea
+            short num1, num2, num3;
+            num1 = 9;
+            num2 = 10;
+            num3 = 5;
+
+            //Modificar el valor que hay guardado en una variable
+            int x1 = 10;
+            //Incrementar en uno el valor de x1
+            x1 = x1 + 1;
+            Console.WriteLine("x1 = " + x1);
+
+            //Por ejemplo: Sumar 1 a la variable num1 de tipo Short
+            num1 =  (short)(num1 + 1); //Conversión de int a short
+            Console.WriteLine("num1 = " + num1);
+
+            //Variables con números decimales
+            double temperatura = 25.6;
+            //Aumentar la temperatura en 10 grados.
+            temperatura = temperatura + 10;
+            Console.WriteLine("Temperatura : " + temperatura);
+            Console.ReadLine();
+            //Programa que a partir de la base y la altura , calcula el área de un triángulo
+            double baseTriangulo, altura, area;
+            baseTriangulo = 9;
+            altura = 8.5;
+            
+            area = (baseTriangulo * altura) / 2;
+
+            Console.WriteLine("Area = " + area);
+
+            Console.ReadLine();
 
         }
     }
---
+++ Tema3/02_LecturaPorTeclado/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_LecturaPorTeclado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa que solicita al usuario DNI y Edad
+            String dni;
+            byte edad;
+
+            Console.WriteLine("Introduce el DNI: ");
+            dni = Console.ReadLine();
+
+            Console.WriteLine("Introduce su edad: ");
+            edad = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("DNI: " + dni);
+            Console.WriteLine("Edad: " + edad);
+            Console.ReadLine();
+
+        }
+    }
+}
