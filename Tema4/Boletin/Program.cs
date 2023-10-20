e577720b138303d63d03e60f3f27304f6bf0cf75
RamonRS <celmon1@gmail.com>
2023-10-20 13:04:34 +0200

    Viernes 20 Octubre

---
+++ Tema4/Boletin/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boletin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ejercicio = 0;
+
+            Console.WriteLine("Introducir ejercicio:4 - 8 ");
+            ejercicio = int.Parse(Console.ReadLine());
+
+            if(ejercicio == 4)
+            {
+                //Pico el ejercicio 4
+                double nota1, nota2, nota3, media;
+
+                Console.WriteLine("Intro nota 1: ");
+                nota1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Intro nota 2: ");
+                nota2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Intro nota 3: ");
+                nota3 = double.Parse(Console.ReadLine());
+                media = (nota1 + nota2 + nota3) / 3;
+
+
+                Console.ReadLine();
+
+
+
+            }
+            else if(ejercicio == 5)
+            {
+                //Pico el ejercicio 5
+            }
+            else
+            {
+                //Te has equivocado de ejercicio
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
