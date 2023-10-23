d661e5b748f5003eb3a47c3844fa6f249a003975
RamonRS <celmon1@gmail.com>
2023-10-23 12:19:13 +0200

    Codigo23Octubre

--- Tema4/Boletin/Program.cs
+++ Tema4/Boletin/Program.cs
@@ -30,6 +30,14 @@ namespace Boletin
                 nota3 = double.Parse(Console.ReadLine());
                 media = (nota1 + nota2 + nota3) / 3;
 
+                if (media < 5)
+                {
+                    Console.WriteLine("Suspenso");
+                }
+                else
+                {
+                    Console.WriteLine("Aprobado");
+                }
 
                 Console.ReadLine();
 
@@ -39,6 +47,80 @@ namespace Boletin
             else if(ejercicio == 5)
             {
                 //Pico el ejercicio 5
+                int n1, n2;
+                n1 = 4;
+                n2 = 1;
+                int suma = n1 + n2;
+
+                if(suma >= 0)
+                {
+                    double raiz = Math.Sqrt(suma);
+                    Console.WriteLine("La raiz es: " + raiz);
+                }
+                else
+                {
+                    Console.WriteLine("No se puede calcular.");
+                }
+
+                else if (ejercicio == 6)
+                {
+                    String password = "hola";
+                    String password2;
+                    Console.WriteLine("Intro password: ");
+                    password2 = Console.ReadLine();
+
+                    if (password.Equals(password2))
+                    {
+                        Console.WriteLine("Iguales");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Distintos");
+                    }
+                }
+                else if (ejercicio == 7)
+                {
+                    int x1, x2;
+                    Console.WriteLine("Intro numero 1 : ");
+                    x1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Intro numero 2: ");
+                    x2 = int.Parse(Console.ReadLine());
+                    long resultado;
+                    if (x1 < x2)
+                    {
+                        resultado = (long)Math.Pow(x2, x1);
+                    }
+                    else
+                    {
+                        resultado = (long)Math.Pow(x1, x2);
+                    }
+                    Console.WriteLine("Resultado" + resultado);
+
+                }
+
+            else if(ejercicio == 8)
+                {
+                    double presion, temperatura;
+
+                    if (presion > 2)
+                    {
+                        Console.WriteLine("Abrir válvula");
+                    }
+                    if (temperatura > 500)
+                    {
+                        Console.WriteLine("Reducir la temperatura");
+                    }
+
+                    if(presion<=2 && temperatura <= 500)
+                    {
+                        Console.WriteLine("Todo en orden");
+                    }
+
+                }
+    
+
+
+                
             }
             else
             {
---
+++ Tema4/SentenciaSwitch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenciaSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char opcion;
+            char continuar;
+
+            do
+            {
+                Console.WriteLine("Escoge una opción: ");
+                Console.WriteLine("a) Saludar");
+                Console.WriteLine("b) Animar");
+                Console.WriteLine("c) Despedir");
+                Console.WriteLine("d) Salir del programa");
+                opcion = Char.Parse(Console.ReadLine());
+
+
+                switch (opcion)
+                {
+                    case 'a':
+                        Console.WriteLine("Hola !!!");
+                        break;
+                    case 'b':
+                        Console.WriteLine("Vamoooos !!!");
+                        break;
+                    case 'c':
+                        Console.WriteLine("Hasta la próxima!!!");
+                        break;
+                    default:
+                        Console.WriteLine("Te has equivocado.");
+                        break; //Opcional
+
+                } // Llave que cierra el switch
+                Console.WriteLine("Quiere volver al menú: s->sí/n->no");
+                continuar = Console.ReadKey().KeyChar;//Para que no espere a que el usuario pulse Enter
+                Console.WriteLine();
+                
+            } // LLave que cierra el bucle do
+            while (continuar=='s');
+        }
+    }
+}
